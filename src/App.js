c28a4b9c71722eebab6a441a72980b2243f01b30
yu1ia <vasilenyuky@gmail.com>
2023-06-30 19:40:23 +0300

    service worker, manifest, session storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { fetchWeather } from './api/fetchWeather';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [value, setValue] = useState("");
+    const [weather, setWeather] = useState(sessionStorage.getItem("weather") ? JSON.parse(sessionStorage.getItem("weather")) : {});
+    
+
+    const search = async (e) => {
+        if (e.key === "Enter") {
+            const data = await fetchWeather(value);
+    
+            setWeather(data);
+            sessionStorage.setItem("weather", JSON.stringify(data));
+            setValue("");
+        }
+    }
+
+    return (
+        <div className="main-container">
+            <input 
+                type="text"
+                className='search'
+                placeholder='Search for a city to get a forecast...'
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                onKeyPress={search} />
+            {
+                weather.main && (
+                    <div className="city">
+                        <h2 className="city-name">
+                            <span>{weather.name}</span>
+                            <sup>{weather.sys.country}</sup>
+                        </h2>
+                        <div className="city-temp">
+                            {Math.round(weather.main.temp)}
+                            <sup>&deg;C</sup>
+                        </div>
+                        <div className="info">
+                            <img 
+                                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} 
+                                alt={weather.weather[0].description} 
+                                className='city-icon' />
+                            <p>{weather.weather[0].description}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default App;
